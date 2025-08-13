df6f985e712bc952447c889924f3b1840927fcfa
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-08-13 14:42:14 -0300

    condicionais

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        byte b = 100;
+        boolean bool = false;
+        String str = "Be";
 
+        if (str.isBlank()) {
+            System.out.println("True");
+        } else if (str == "Be"){
+            System.out.println("Be");
+        } else {
+            System.out.println("False");
+        }
     }
 
 }
