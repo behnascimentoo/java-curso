a108eae82e091db277552502a1eef699dbe64e30
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-08-13 14:48:51 -0300

    vetores

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         boolean bool = false;
         String str = "Be";
 
+        int[] colecaoDeInteiros= {1, 2, 3, 4, 5, 6666}; //cria um vetor ja com os valores declarados
+        int[] meusNumeros = new int[5]; //cria um vetor saber os valores de dentro, apenas com o tamanho
+
+
         if (str.isBlank()) {
             System.out.println("True");
         } else if (str == "Be"){
@@ -11,6 +15,8 @@ public class Main {
         } else {
             System.out.println("False");
         }
+
+        System.out.println(colecaoDeInteiros.length);
     }
 
 }
